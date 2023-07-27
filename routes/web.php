b68b3869c8abd5217a3cc7c620313863dc28b97c
Mariana304 <marianaortegaortiz92@gmail.com>
2023-07-27 09:45:07 -0500

    He creado la ruta home y la ruta tipo resourse

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+
+Route::resource('tasks',TasksController::class);
