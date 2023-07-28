69bb51dcdc8ad720b2315950ac40f52f9cbeac4f
Mariana304 <marianaortegaortiz92@gmail.com>
2023-07-28 15:37:12 -0500

    Arreglo del daño en la vista show en la rama main

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -6,7 +6,112 @@
 
 @section('contect')
 
-    <h1> {{ $task->task }} </h1>
+
+    <div class="container w-96 text-center h-auto p-9 mx-auto ">
+
+        <div class="grid justify-items-start mt-2 ">
+
+            <p class=" flex flex-col   w-16 ">
+                <a href="{{ route('tasks.index') }}" class="{{ request()->routeIs('tasks.*') ? 'active' : '' }}">
+                    <img src="{{ asset('images/volver.png') }}" class="w-10  ">
+                </a>
+            </p>
+
+        </div>
+
+
+        <div class=" uppercase justify-items-center text-2xl italic  font-semibold">
+            <h1>{{ $task->task }}</h1>
+        </div>
+
+
+
+
+        <div class=" mx-18 w-64 justify-items-center border mt-3  ">
+            <table class="text-sm w-full table-auto  text-center text-gray-500 ">
+                <thead class="text-xs w-10 text-gray-300 uppercase   bg-gray-800  ">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 ">
+                            Tarea
+                        </th>
+
+                    </tr>
+                </thead>
+
+
+                <tbody>
+                    <tr class=" bg-white even:bg-slate-50 text-base  border border-gray-300 ">
+                        <th scope="row" class="px-6 py-4 font-medium  italic text-gray-900 whitespace-nowrap ">
+                            {{ $task->task }}
+                        </th>
+
+                    </tr>
+                </tbody>
+
+
+            </table>
+
+        </div>
+
+
+
+        <div class=" mx-18 w-64 justify-items-center border mt-3  ">
+            <table class="text-sm w-full table-auto  text-center text-gray-500 ">
+                <thead class="text-xs w-10 text-gray-300 uppercase   bg-gray-800  ">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 ">
+                            Descripcion
+                        </th>
+
+
+                    </tr>
+                </thead>
+
+
+                <tbody>
+                    <tr class=" bg-white even:bg-slate-50 text-base  border  border-gray-300 ">
+                        <th scope="row" class="px-6 py-4 font-medium  italic text-gray-900 whitespace-nowrap ">
+                            {{ $task->summary }}
+                        </th>
+
+                    </tr>
+                </tbody>
+
+
+            </table>
+
+        </div>
+
+
+
+        <div class=" mx-18 w-64 justify-items-center border mt-3  ">
+            <table class="text-sm w-full table-auto  text-center text-gray-500 ">
+                <thead class="text-xs w-10 text-gray-300 uppercase   bg-gray-800  ">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 ">
+                            Estado
+                        </th>
+
+
+                    </tr>
+                </thead>
+
+
+                <tbody>
+                    <tr class=" bg-white even:bg-slate-50 text-base  border  border-gray-300 ">
+                        <th scope="row" class="px-6 py-4 font-medium  italic text-gray-900 whitespace-nowrap ">
+                            {{ $task->state }}
+                        </th>
+
+                    </tr>
+                </tbody>
+
+
+            </table>
+
+        </div>
+
+
 
 
 
@@ -79,12 +184,19 @@
 
                                 @method('delete')
 
-        <button type="submit">Eliminar</button>
-        
-    </form>
+                                <button type="submit">
+                                    <img class="w-14 h-12 mt-1 mx-5 " title="Eliminar"
+                                        src="{{ asset('images/trash.jpg') }}">
+                                </button>
+                            </form>
+                        </td>
+
+                    </tr>
+                </tbody>
 
 
             </table>
 
-  
-@endsection
+        </div>
+
+    @endsection
